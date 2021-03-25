135966ff5de1126d26cf39d035e4265ac5413dd1
Quangchien99 <phamquangchien170499@gmail.com>
2021-03-25 12:59:27 +0700

    Restore app state after process death using the SavedStateHandle inside a viewmodel

--- app/src/main/java/com/example/imagesearchapplication/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/example/imagesearchapplication/ui/gallery/GalleryViewModel.kt
@@ -1,22 +1,30 @@
 package com.example.imagesearchapplication.ui.gallery
 
+import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.switchMap
 import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
 import com.example.imagesearchapplication.data.UnsplashRepository
-import com.example.imagesearchapplication.utils.utilConsts.Const
 
-class GalleryViewModel @ViewModelInject constructor(private val unsplashRepository: UnsplashRepository) :
-    ViewModel() {
-    private val currentQuery = MutableLiveData(Const.DEFAULT_QUERY)
+class GalleryViewModel @ViewModelInject constructor(
+    private val unsplashRepository: UnsplashRepository,
+    @Assisted state: SavedStateHandle
+) : ViewModel() {
+
+    private val currentQuery = state.getLiveData(CURRENT_QUERY, DEFAULT_QUERY)
     val photos = currentQuery.switchMap { queryString ->
         unsplashRepository.getSearchResource(queryString).cachedIn(viewModelScope)
     }
 
     fun searchPhotos(query: String) {
         currentQuery.value = query
+    }
+
+    companion object {
+        private const val CURRENT_QUERY = "current_query"
+        private const val DEFAULT_QUERY = "cats"
     }
 }
